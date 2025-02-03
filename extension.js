3a2c78bbde6f1814f7ad8071b8b277492aa6dcd1
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2025-02-03 22:19:48 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,11 +1,11 @@
 import St from 'gi://St';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import GLib from 'gi://GLib';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import GObject from 'gi://GObject';
 import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
 import Gio from 'gi://Gio';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 let domainToPing = 'google.com'; // Change this to the domain or IP address you want to ping
 let timeoutId = null;
@@ -16,6 +16,8 @@ const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
         _init() {
             super._init(0.0, 'My Indicator');
+
+            // Create a label to display the ping result
             this._label = new St.Label({
                 text: '',
                 y_align: Clutter.ActorAlign.CENTER
@@ -26,15 +28,19 @@ const Indicator = GObject.registerClass(
             this._entryItem = new PopupMenu.PopupMenuItem('');
             this._entryItem.actor.reactive = false;
             this._entryItem.actor.can_focus = false;
+
             this._entry = new St.Entry({
                 hint_text: 'Enter domain or IP address',
                 track_hover: true,
                 can_focus: true
             });
+
+            // Update domainToPing when the user presses Enter
             this._entry.clutter_text.connect('activate', () => {
                 domainToPing = this._entry.get_text();
                 this.menu.close();
             });
+
             this._entryItem.add_child(this._entry);
             this.menu.addMenuItem(this._entryItem);
 
@@ -45,18 +51,21 @@ const Indicator = GObject.registerClass(
             });
             this.menu.addMenuItem(this._soundToggle);
 
+            // Start the ping monitoring loop
             this.checkPingAsync();
         }
 
         async checkPingAsync() {
             while (true) {
                 try {
+                    // Execute the ping command
                     let out = await new Promise((resolve, reject) => {
                         let proc = new Gio.Subprocess({
                             argv: ['ping', '-c', '1', '-W', '2', domainToPing],
                             flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
                         });
                         proc.init(null);
+
                         proc.communicate_utf8_async(null, null, (proc, res) => {
                             try {
                                 let [ok, stdout, stderr] = proc.communicate_utf8_finish(res);
@@ -70,32 +79,37 @@ const Indicator = GObject.registerClass(
                             }
                         });
                     });
+
+                    // Parse the ping output
                     let match = /time=([\d.]+)/.exec(out);
                     let currentStatus = match ? `${Math.round(parseFloat(match[1]))} ms` : 'No response';
 
-                    // Update label
+                    // Update the label with the current status
                     this._label.set_text(currentStatus);
 
                     // Check for state changes between "ms" and "No response"
                     if (soundEnabled && this.isStatusChangeSignificant(lastStatus, currentStatus)) {
-                        this.playSound();
+                        this.playSound(currentStatus);
                     }
 
-                    // Update last status
+                    // Update the last status
                     lastStatus = currentStatus;
 
                 } catch (e) {
+                    // Handle errors (e.g., no response)
                     let currentStatus = 'No response';
                     this._label.set_text(currentStatus);
 
                     // Check for state changes between "ms" and "No response"
                     if (soundEnabled && this.isStatusChangeSignificant(lastStatus, currentStatus)) {
-                        this.playSound();
+                        this.playSound(currentStatus);
                     }
 
-                    // Update last status
+                    // Update the last status
                     lastStatus = currentStatus;
                 }
+
+                // Wait for 1 second before the next ping
                 await new Promise(resolve => timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, resolve));
             }
         }
@@ -107,17 +121,27 @@ const Indicator = GObject.registerClass(
             return isPreviousSuccess !== isCurrentSuccess;
         }
 
-        playSound() {
-            // Play a sound file (replace with the path to your sound file)
-            let soundFile = Gio.File.new_for_path('/usr/share/sounds/gnome/default/alerts/click.ogg');
+        playSound(currentStatus) {
+            // Get the sound player
+            let player = global.display.get_sound_player();
+            if (!player) {
+                console.error('Sound player not available');
+                return;
+            }
+
+            // Construct the path to the sound files
+            const HomePath = GLib.get_home_dir();
+            const ExtensionPath = `${HomePath}/.local/share/gnome-shell/extensions/ping@prasanthc41m.github.com/`;
+            const soundFilePath = currentStatus.endsWith(' ms')
+                ? `${ExtensionPath}ping_started.ogg`
+                : `${ExtensionPath}no_response.ogg`;
+
+            // Play the sound file
+            let soundFile = Gio.File.new_for_path(soundFilePath);
             if (soundFile.query_exists(null)) {
-                let player = new Gio.Subprocess({
-                    argv: ['paplay', soundFile.get_path()],
-                    flags: Gio.SubprocessFlags.NONE,
-                });
-                player.init(null);
+                player.play_from_file(soundFile, 'Ping Alert', null);
             } else {
-                log('Sound file not found: ' + soundFile.get_path());
+                console.error(`Sound file not found: ${soundFilePath}`);
             }
         }
     }
@@ -128,16 +152,22 @@ let indicator;
 export default class PingExtension {
 
     enable() {
+        // Create and add the indicator to the panel
         indicator = new Indicator();
         Main.panel.addToStatusArea('indicator', indicator);
     }
 
     disable() {
+        // Clean up resources
         if (timeoutId) {
             GLib.Source.remove(timeoutId);
             timeoutId = null;
         }
-        indicator.destroy();
-        indicator = null; // Set indicator to null
+
+        // Destroy the indicator
+        if (indicator) {
+            indicator.destroy();
+            indicator = null;
+        }
     }
 }
